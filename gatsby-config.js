dc9efc5843955d8f8ffb710763a9049a8e1ae5ab
Tripurari Shankar <tripurari.shankar@cred.club>
2021-09-01 16:37:19 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify-cms`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
